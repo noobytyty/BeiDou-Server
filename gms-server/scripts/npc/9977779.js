ff56c32001b17b46e1698735ec8e8ecd5c0e8d7f
noobytyty <s4560449ytyty@gmail.com>
2026-08-19 21:53:15 +0800

    拆分NPC：9977777词条工匠、9977778美容师（美容+存档）、9977779收藏家（图鉴）

---
+++ gms-server/scripts/npc/9977779.js
@@ -0,0 +1,111 @@
+/**北斗脚本
+
+收藏家 9977779
+
+收藏图鉴：怪物卡 + 任务达人进度、领取收藏家腰带与成就勋章。
+*/
+var collectionMode = -1;
+var collSvc = Java.type("org.gms.server.CollectionService").getInstance();
+var COLLECTOR_BELT = 1132991;
+var ACHIEVEMENTS = [
+    [1142992, 25, 0, "卡牌新星"],       // 收集 25 张怪物卡
+    [1142993, 50, 0, "卡牌大师"],       // 收集 50 张怪物卡
+    [1142994, 100, 0, "卡牌收藏家"],    // 收集 100 张怪物卡
+    [1142995, 0, 50, "任务新秀"],       // 完成 50 个任务
+    [1142996, 0, 100, "任务达人"],      // 完成 100 个任务
+    [1142997, 0, 200, "任务大师"],      // 完成 200 个任务
+    [1142998, 100, 100, "收藏大师"]     // 100 卡 + 100 任务
+];
+
+function collectionMenu() {
+    var accId = cm.getPlayer().getAccountId();
+    var cards = collSvc.getCardCount(accId);
+    var quests = collSvc.getQuestCount(accId);
+    cm.sendSimple("收藏图鉴：\r\n怪物卡：" + cards + " 张（每 25 张全属性+1）\r\n完成任务：" + quests + " 个（每 50 个全属性+1、攻+1、HP/MP+100）\r\n\r\n#L0#查看当前加成#l\r\n#L1#领取收藏家腰带（" + COLLECTOR_BELT + "）#l\r\n#L2#领取成就勋章#l\r\n#L3#返回");
+}
+
+function collectionAction(selection) {
+    var accId = cm.getPlayer().getAccountId();
+    if (selection == 3) {
+        collectionMode = -1;
+        collectionMenu();
+        return;
+    }
+    if (selection == 0) {
+        // 查看加成
+        var bonus = collSvc.getBonus(accId);
+        var msg = "当前收藏加成（穿戴收藏家腰带时生效）：\r\n";
+        msg += "力量 +" + bonus.str + "，敏捷 +" + bonus.dex + "，智力 +" + bonus.int_ + "，运气 +" + bonus.luk + "\r\n";
+        msg += "物攻 +" + bonus.watk + "，魔攻 +" + bonus.matk + "，HP +" + bonus.maxhp + "，MP +" + bonus.maxmp;
+        cm.sendOk(msg);
+        collectionMode = -1;
+        cm.dispose();
+        return;
+    }
+    if (selection == 1) {
+        // 领取收藏家腰带
+        if (cm.haveItem(COLLECTOR_BELT)) {
+            cm.sendOk("你已经拥有收藏家腰带了！");
+        } else if (!cm.canHold(COLLECTOR_BELT)) {
+            cm.sendOk("背包空间不足！");
+        } else {
+            cm.gainItem(COLLECTOR_BELT, 1);
+            cm.sendOk("已领取收藏家腰带 #t" + COLLECTOR_BELT + "#！穿戴后按你的收藏进度获得属性加成。");
+        }
+        collectionMode = -1;
+        cm.dispose();
+        return;
+    }
+    if (selection == 2) {
+        // 领取成就勋章
+        var cards = collSvc.getCardCount(accId);
+        var quests = collSvc.getQuestCount(accId);
+        var got = [];
+        var failed = [];
+        for (var i = 0; i < ACHIEVEMENTS.length; i++) {
+            var a = ACHIEVEMENTS[i];
+            var itemId = a[0];
+            if (cards >= a[1] && quests >= a[2]) {
+                if (cm.haveItem(itemId)) {
+                    failed.push("#t" + itemId + "#（已拥有）");
+                } else if (!cm.canHold(itemId)) {
+                    failed.push("#t" + itemId + "#（背包不足）");
+                } else {
+                    cm.gainItem(itemId, 1);
+                    got.push("#t" + itemId + "#");
+                }
+            } else {
+                failed.push("#t" + itemId + "#（需卡 " + a[1] + "/任务 " + a[2] + "）");
+            }
+        }
+        var msg = "成就勋章领取结果：\r\n";
+        if (got.length > 0) { msg += "已领取：" + got.join("、") + "\r\n"; } else { msg += "没有新达成的成就勋章。\r\n"; }
+        if (failed.length > 0) { msg += "未达成：" + failed.join("、"); }
+        cm.sendOk(msg);
+        collectionMode = -1;
+        cm.dispose();
+        return;
+    }
+    collectionMode = -1;
+    collectionMenu();
+}
+
+// ===== 高级美容服务结束 =====
+
+
+function start() {
+    collectionMode = 1;
+    collectionMenu();
+}
+
+function action(mode, type, selection) {
+    if (mode <= 0) {
+        cm.dispose();
+        return;
+    }
+    if (collectionMode > 0) {
+        collectionAction(selection);
+        return;
+    }
+    cm.dispose();
+}
